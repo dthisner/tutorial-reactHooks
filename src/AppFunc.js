c1d4b3f0f7078b38ccddf22a46d564d6831d630b
Dennis Thisner <dennis@applydigital.co>
2020-08-10 18:00:00 -0700

    are you online check

--- src/AppFunc.js
+++ src/AppFunc.js
@@ -4,15 +4,28 @@ const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(true);
 
   useEffect(() => {
     document.title = `You have clicked ${count} times`;
     window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
     };
   }, [count]);
+
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
 
   const handleMouseMove = (event) => {
     setMousePosition({
@@ -50,6 +63,10 @@ const App = () => {
       ></div>
       <h2>Mouse Position</h2>
       <p>{JSON.stringify(mousePosition, null, 2)} </p>
+      <h2>Online</h2>
+      <p>
+        You are <strong>{status ? "online" : "false"}</strong>
+      </p>
     </>
   );
 };
